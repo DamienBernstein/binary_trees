f02bf49a995af028a2af8799e65f97ec493c8dc0
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-03-01 19:10:53 +0200

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -28,10 +28,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		l = (tree->left) ? 1 + binaey_tree_height(tree->left) : 1;
+		l = (tree->left) ? 1 + binary_tree_height(tree->left) : 1;
 
 		r = (tree->right) ? 1 + binary_tree_height(tree->right) : 1;
-		return (l > r ? 1 : r);
+		return (l > r ? l : r);
 	}
 	return (0);
 }
