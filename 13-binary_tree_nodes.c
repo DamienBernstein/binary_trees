bb9810f30eecd4ae662892945547c0c42c84bc5e
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-03-01 19:00:42 +0200

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,9 +12,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		size += (tree->left || tree->right) ? 1 : 0;
-		size += binary_tree_nodes(tree->left);
-		size += binary_tree_nodes(tree->right);
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
 	}
 	return (nodes);
 }
